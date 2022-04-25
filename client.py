77e95c919257025fc77cd5690ed62506a499a003
Guillermo <guillermo.garcia.espuela@gmail.com>
2022-04-25 17:12:04 +0200

    Adapt client to produce endless data

--- client.py
+++ client.py
@@ -2,19 +2,29 @@ from datetime import datetime
 
 from bs4 import BeautifulSoup
 import requests
-def createMessage(fx):
-    url = 'https://finance.yahoo.com/quote/' + fx.replace('/', '') + '%3DX'
+def createMessage(unique_id, price):
+    url = 'https://finance.yahoo.com/quote/' + price.replace('/', '') + '%3DX'
     headers = {"User-Agent":"Mozilla/5.0"}
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.text, 'lxml')
     current_time = datetime.utcnow().strftime('%d-%M-%Y %H:%M:%S:%f')[:-3]
     bid = soup.find('td', {'data-test':'BID-value'}).text
     ask = soup.find('td', {'data-test':'ASK-value'}).text
-    price_feed =  ",".join([fx, bid, ask, current_time])
+    price_feed =  ",".join([str(unique_id), price, bid, ask, current_time])
     print(price_feed)
     return price_feed
-      
+
+def sendMessages():
+    prices_list = ['EUR/USD', 'EUR/GBP', 'EUR/JPY', 'EUR/CHF', 'GBP/USD', 'USD/CNY']
+    unique_id = 0
+    
+    while True:
+        for p in prices_list:           
+
+            createMessage(unique_id, p)
+
+            unique_id = unique_id + 1
+            
+            
 if __name__ == '__main__':
-    fx = 'EUR/USD'
-    createMessage(fx)
-      
+    sendMessages()
